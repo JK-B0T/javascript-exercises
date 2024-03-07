0ec2cef92620399baec87efa711172882ab04ddd
JK-B0T <JK-B0T@users.noreply.github.com>
2024-03-07 12:52:39 +0100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let modifiedArray = array;
+    const args = Array.from(arguments);
+    let arrayLenght = array.length;
 
+    for (let i = 0; i < arrayLenght; i++) {
+        for (let j = 1; j < args.length; j++) {
+            if (modifiedArray[i] === args[j]) {
+                modifiedArray = modifiedArray.toSpliced(i, 1);
+                arrayLenght--;
+                i--;
+                continue;
+            }
+        }
+    }
+    return modifiedArray;
 };
+
+removeFromArray([1,2,3,4], 2 ,3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
