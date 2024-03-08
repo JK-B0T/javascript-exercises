2f29d8ae158e7499e149c22a406d10f6786f8cb5
JK-B0T <JK-B0T@users.noreply.github.com>
2024-03-08 09:51:34 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let largerNum = 0;
+    let smallestNum = 0;
+    let sum = 0;
+    if (typeof num1 != "number" || typeof num2 != "number" || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else {
+        if (num1 < num2) {
+            largerNum = num2;
+            smallestNum = num1;
+        } else {
+            largerNum = num1;
+            smallestNum = num2;
+        }
+
+        for (let i = smallestNum; i <= largerNum; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 
 };
 
