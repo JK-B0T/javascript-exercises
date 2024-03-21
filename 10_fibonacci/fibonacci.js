937228d97c2125306bdd5eff71dac7ee9a0ccdac
JK-B0T <JK-B0T@users.noreply.github.com>
2024-03-21 09:25:05 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (+num === 0) {
+        return 0;
+    } else if (+num < 0) {
+        return "OOPS";
+    } else {
+        let lastNum = 1
+        let currentNum = 1;
+        let tempNum = 0;
+    
+        for (let i = 2; i < +num; i++) {
+            tempNum = currentNum;
+            currentNum += lastNum;
+            lastNum = tempNum;
+        }
+        return currentNum;
+    }
 };
-
+console.log(fibonacci(4))
 // Do not edit below this line
 module.exports = fibonacci;
