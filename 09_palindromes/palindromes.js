a36961afad0fc4da6ab2ec55212218889d2f3803
JK-B0T <JK-B0T@users.noreply.github.com>
2024-03-21 09:13:25 +0100

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,25 @@
 const palindromes = function (string) {
+    const ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let cleanString = string;
+    let reverseString = string;
 
+    cleanString = string.toLowerCase()
+    .split("") 
+    .filter((char) => ALPHANUMERIC.includes(char))
+    .join("");
 
+    reverseString = reverseString.toLowerCase()
+    .split("") 
+    .filter((char) => ALPHANUMERIC.includes(char))
+    .reverse()
+    .join("")
+
+    if (cleanString === reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+palindromes('Hola buenas')
 // Do not edit below this line
 module.exports = palindromes;
